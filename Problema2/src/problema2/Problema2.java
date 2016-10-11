b11d4634d72b32683b07d2266c73c160c9fe8f34
franc <franc@Pacopa-MSI>
2016-10-10 20:49:17 -0500

    Problema2

---
+++ Problema2/src/problema2/Problema2.java
@@ -0,0 +1,40 @@
+
+package problema2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author franc
+ */
+public class Problema2 {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double metro;
+        metro = pedirDato();
+        mostrar(metro);
+        
+    }
+    
+    public static double pedirDato(){
+        double metro;
+        System.out.println("*** Convertidor de metros a pies, millas y yardas *** ");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca la cantidad que desea convertir: ");
+       metro = teclado.nextDouble();
+        return metro;
+    }
+    
+    public static void mostrar(double metro){
+        double pie,milla,yarda;
+        pie = metro * 3.28084;
+        milla = metro * 5280;
+        yarda = metro * 3;
+        System.out.println(metro + " metros es " + pie +" pies");
+        System.out.println(metro + " metros es " + milla +" millass");
+        System.out.println(metro + " metros es " + yarda +" yardas");
+        
+    }
+}
