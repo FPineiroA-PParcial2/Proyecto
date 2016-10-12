fe287dc27658b5e4c6525b67bacb6e30114dea7f
franc <franc@Pacopa-MSI>
2016-10-11 19:23:14 -0500

    Problema 4

---
+++ Problema4/src/problema4/Problema4.java
@@ -0,0 +1,33 @@
+
+package problema4;
+import java.util.*;
+/**
+ * @author franc
+ */
+public class Problema4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float far;
+        far = pedirDato();
+        resultado(far);
+    }
+    
+    public static float pedirDato(){
+        float far;
+        System.out.println(" *** Calculadora de grados °F a partir de °C ***");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el número que deseas convertir: ");
+        far = teclado.nextFloat();
+        return far;
+    }
+    
+    public static void resultado(float far){
+        float respuesta;
+        respuesta = (far*9)/5 + 32;
+        System.out.println(far+"°F en celcius es "+ respuesta +"°C");
+    }
+}
