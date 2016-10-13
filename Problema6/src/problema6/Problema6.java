98e2111d48c5ef0553b0834717ee049eebbefd38
Pacopa97 <franciscopineiroa@hotmail.com>
2016-10-13 12:10:05 -0500

    Update Problema6.java

--- Problema6/src/problema6/Problema6.java
+++ Problema6/src/problema6/Problema6.java
@@ -15,7 +15,7 @@ public class Problema6 {
         calcularSueldo(hora,sueldo);
     }
     
-    public static int pedirHoras(){
+    public static int pedirHoras(){ //Se pide que se introduzcan las horas trabajadas
         int hora;
         System.out.println(" *** Calculadora de sueldo semanal *** ");
         Scanner teclado = new Scanner(System.in);
@@ -23,14 +23,14 @@ public class Problema6 {
         hora = teclado.nextInt();
         return hora;
     }
-    public static float pedirSueldo(){
+    public static float pedirSueldo(){  //Se pide que se introduzca el saldo por hora
         float sueldo;
          Scanner teclado = new Scanner(System.in);
          System.out.println("Introduce la paga por hora: ");
         sueldo= teclado.nextFloat();
         return sueldo;
     }
-    public static void calcularSueldo(int hora, float sueldo){
+    public static void calcularSueldo(int hora, float sueldo){  //Se calcula el sueldo tomando en cuenta las condiciones establecidas
         float respuesta = 0;
         if(hora<=40){
             respuesta =hora*sueldo;
@@ -40,7 +40,7 @@ public class Problema6 {
             respuesta=(40*sueldo)+(8)*(sueldo*2)+(hora-48)*(sueldo*3);
         }
         
-        System.out.println("El sueldo ganado es de: " + respuesta);   
+        System.out.println("El sueldo ganado es de: " + respuesta);   //Se muestra el sueldo ganado
     }
     
 }
