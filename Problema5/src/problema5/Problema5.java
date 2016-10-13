90590799f64cfc25419c1462b9b7e90114e8954d
Pacopa97 <franciscopineiroa@hotmail.com>
2016-10-13 12:08:16 -0500

    Update Problema5.java

--- Problema5/src/problema5/Problema5.java
+++ Problema5/src/problema5/Problema5.java
@@ -15,7 +15,7 @@ public class Problema5 {
         comprobar(primo);
     }
     
-    public static int introducir(){
+    public static int introducir(){ //Se  pide el dato a comprobar
         int primo;
         System.out.println(" *** Calculadora de números primos ***");
         Scanner teclado = new Scanner(System.in);
@@ -24,13 +24,13 @@ public class Problema5 {
         return primo;
     }
     
-    public static void comprobar(int primo){
+    public static void comprobar(int primo){ //Se hace la operacion para comprobar el numero
         int i=1;
         while(primo%i ==0){
             i++;
         }
         if(i==2){
-            System.out.println("El número "+ primo+ " es primo");
+            System.out.println("El número "+ primo+ " es primo"); //Se muestra si es o no primo
         }else{
             System.out.println("El número "+ primo+ " NO es primo");
         }
